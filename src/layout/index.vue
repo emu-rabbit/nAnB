629bd6c2bedd7a984e9caa5db4d2a1da21b55126
immortalmice <chunyi_cho@aprplanet.com.tw>
2022-05-30 14:20:08 +0800

    Feat: Basic layout & router

---
+++ src/layout/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>layout</div>
+  <router-view />
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/layout/index.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    redirect: '/tutorial',
+    component: Layout,
+    children: [{
+      path: 'tutorial',
+      component: () => import('@/views/Tutorial.vue')
+    }]
   }
 ]
 
